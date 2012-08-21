a7306c61673e3d6f2e11e5a6b370e8dae262dd5c
Andy Meneely <andy.meneely@gmail.com>
2012-08-21 19:16:08 -0400

    three player and two player combinations in fitness function

--- src/org/chaoticbits/gofirst/genetic/SimulationEvaluator.java
+++ src/org/chaoticbits/gofirst/genetic/SimulationEvaluator.java
@@ -31,6 +31,13 @@ public class SimulationEvaluator implements IFitnessEvaluator {
 
 	private long[] simulate(List<Die> dice) {
 		long[] victories = new long[] { 0, 0, 0, 0 };
+		fourPlayer(dice, victories);
+		threePlayer(dice, victories);
+		twoPlayer(dice, victories);
+		return victories;
+	}
+
+	private void fourPlayer(List<Die> dice, long[] victories) {
 		for (long trial = 0; trial < numTrials; trial++) {
 			int victor = -1;
 			int highest = -1;
@@ -43,14 +50,62 @@ public class SimulationEvaluator implements IFitnessEvaluator {
 			}
 			victories[victor]++;
 		}
-		return victories;
+	}
+
+	private void threePlayer(List<Die> dice, long[] victories) {
+		for (long trial = 0; trial < numTrials; trial++) {
+			for (int playerOut = 0; playerOut < victories.length; playerOut++) {
+				int victor = -1;
+				int highest = -1;
+				for (int player = 0; player < dice.size(); player++) {
+					if (player != playerOut) { // skip over one player
+						int roll = dice.get(player).roll(rand);
+						if (roll > highest) {
+							victor = player;
+							highest = roll;
+						}
+					}
+				}
+				victories[victor]++;
+			}
+		}
+	}
+
+	private void twoPlayer(List<Die> dice, long[] victories) {
+		for (long trial = 0; trial < numTrials; trial++) {
+			for (int playerOut1 = 0; playerOut1 < victories.length - 1; playerOut1++) {
+				for (int playerOut2 = playerOut1 + 1; playerOut2 < victories.length; playerOut2++) {
+					int victor = -1;
+					int highest = -1;
+					for (int player = 0; player < dice.size(); player++) {
+						if (player != playerOut1 && player != playerOut2) { // skip over two players
+							int roll = dice.get(player).roll(rand);
+							if (roll > highest) {
+								victor = player;
+								highest = roll;
+							}
+						}
+					}
+					victories[victor]++;
+				}
+			}
+		}
 	}
 
 	private Double compute(long[] victories, int numDice) {
 		double totalMisses = 0;
+		long totalVictories = sum(victories);
 		for (int victor = 0; victor < numDice; victor++) {
-			totalMisses += Math.abs(victories[victor] - numTrials / numDice); // |actual-expected|
+			totalMisses += Math.abs(victories[victor] - totalVictories / numDice); // |actual-expected|
 		}
-		return 1.0 - totalMisses / (2 * numTrials); // double-counted the totalMisses, hence the 2
+		return 1.0 - totalMisses / (2 * totalVictories); // double-counted the totalMisses, hence the 2
+	}
+
+	private long sum(long[] victories) {
+		long total = 0;
+		for (long victory : victories) {
+			total += victory;
+		}
+		return total;
 	}
 }
--- test/org/chaoticbits/gofirst/genetic/SimulationEvaluatorTest.java
+++ test/org/chaoticbits/gofirst/genetic/SimulationEvaluatorTest.java
@@ -13,7 +13,7 @@ public class SimulationEvaluatorTest {
 		// normal means: die[0] < die[1] < die[2] < die[3]
 		// which means that it's really, really lopsided - near zero
 		Double fitness = new SimulationEvaluator(new Random(123)).fitness(new DiceGenome(DiceGenomeTest.NORMAL));
-		assertEquals("fitness is terrible, only 25% right", 0.25, fitness, 0.001);
+		assertEquals("fitness is terrible, only 60% right", 0.60, fitness, 0.01);
 		for (int i = 1; i <= 12; i++) {
 			System.out.print(i + ", " + (i + 12) + ", " + (i + 24) + ", " + (i + 36) + ",");
 		}
@@ -23,6 +23,6 @@ public class SimulationEvaluatorTest {
 	public void testComputeInterleaved() throws Exception {
 		// interleaved is a little better
 		Double fitness = new SimulationEvaluator(new Random(123)).fitness(new DiceGenome(DiceGenomeTest.INTERLEAVED));
-		assertEquals("fitness is terrible, about 83% close", 0.832, fitness, 0.001);
+		assertEquals("fitness is better, about 89% close", 0.894, fitness, 0.001);
 	}
 }
